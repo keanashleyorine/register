1591cd113aebb6888e979c9fed89cb3cbcbe7f5d
keanashleyorine <orinekeanashley@gmail.com>
2024-03-04 09:33:23 +0800

    added

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
-const username = document.querySelector('username')
-const password = document.querySelector('password')
-const confirmPassword = document.querySelector('confirm-password')
-const age = document.querySelector('age')
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirm-password')
+const age = document.querySelector('#age')
 
 const button = document.querySelector('#age')
 button.onclick = () => {
